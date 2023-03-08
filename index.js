d0c48e518588627710c536b4b6e4a5b014e23df9
Alabi <alabiemmanuel177@gmail.com>
2023-03-08 19:06:49 +0100

    edited auth

--- index.js
+++ index.js
@@ -9,11 +9,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const whitelist = [
-  "https://bucodel-lms.onrender.com",
   "http://localhost:3000",
-  "https://bucodel.vercel.app",
-  "https://bucodel-alabiemmanuel177.vercel.app",
-  "https://bucodel.vercel.app/",
 ];
 const corsOptions = {
   origin: function (origin, callback) {
