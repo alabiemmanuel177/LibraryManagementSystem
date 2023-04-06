c5f8f068e4e6d755a7b7f1d6ea0fd6e6f5c3e28f
Alabi <alabiemmanuel177@gmail.com>
2023-04-06 23:08:40 +0100

    CORS FIxes

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ const express = require("express");
 const app = express();
 const connectDB = require("./migrations/index.js");
 const cors = require("cors");
-const server = require("http").createServer(app);
-const io = require("socket.io")(server, { cors: { origin: "*" } });
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -32,7 +30,7 @@ const corsOptions = {
   },
 };
 
-// app.use(cors(corsOptions));
+app.use(cors(corsOptions));
 app.use(cors());
 
 const { routes } = require("./routes/main");
