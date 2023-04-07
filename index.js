3887c37647df03a6fc7ad3391d970935b20e09ce
Alabi <alabiemmanuel177@gmail.com>
2023-04-07 09:34:56 +0100

    CORS TRIAL TEST V1

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const app = express();
 const connectDB = require("./migrations/index.js");
-const cors = require("cors");
 const server = require("http").createServer(app);
 const io = require("socket.io")(server, { cors: { origin: "*" } });
 
@@ -21,22 +20,17 @@ if (!fs.existsSync(UPLOADS_DIR)) {
   console.log("Folder Exist");
 }
 
-const allowedOrigins = ["http://localhost:3000", "http://example.com"];
-
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      // Check if the request origin is in the allowed origins array
-      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-  })
-);
-
 const { routes } = require("./routes/main");
+
+// Add the following middleware to enable CORS
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 // Registers routes
 routes({ app, io });
